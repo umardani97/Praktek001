a3b9a3d6c2c36741651dc14c4d378047a5f4d6c7
umardani97 <32365705+umardani97@users.noreply.github.com>
2018-03-28 21:53:48 +0800

    Mengisi nilai atribut

--- src/praktek001/MahasiswaAksi.java
+++ src/praktek001/MahasiswaAksi.java
@@ -12,5 +12,42 @@ package praktek001;
 public class MahasiswaAksi {
     public static void main(String[] args) {
         Mahasiswa Kendil = new Mahasiswa();
+        Kendil.nama="Kendil";
+        Kendil.NPM="1663009";
+        Kendil.jurusan="Ekonomi";
+        Kendil.nohp="089867575";
+        Kendil.IPK=3.4;
+        
+        Mahasiswa Zilong = new Mahasiswa();
+        
+        Zilong.nama="Zilong";
+        Zilong.NPM="16639867";
+        Zilong.jurusan="Sastra";
+        Zilong.nohp="09643246";
+        Zilong.IPK=2.9;
+        
+        Mahasiswa Miya = new Mahasiswa();
+        
+        Miya.nama="Miya";
+        Miya.NPM="1209867";
+        Miya.jurusan="Agama";
+        Miya.nohp="08765321";
+        Miya.IPK=3.1;
+        
+        Mahasiswa Lolita = new Mahasiswa();
+        
+        Lolita.nama="Lolita";
+        Lolita.NPM="1308973";
+        Lolita.jurusan="Olahraga";
+        Lolita.nohp="08123456";
+        Lolita.IPK=4.4;
+        
+        Mahasiswa Akay = new Mahasiswa();
+        
+        Akay.nama="Akay";
+        Akay.NPM="11008877";
+        Akay.jurusan="Game";
+        Akay.nohp="0088776543";
+        Akay.IPK=4.5;
     }
 }
